dd453a635feb77c874032c5c0fac3f84b050b380
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-14 16:10:30 +0200

    Hash password with bcryptjs

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
     name: {
@@ -23,5 +24,34 @@ const userSchema = new Schema({
     stripe_seller: {},
     stripeSession: {}
 }, { timestamps: true });
+
+// Middleware to hash the password before saving the user
+userSchema.pre('save', async function(next) {
+    let user = this;
+
+    // Only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    try {
+        // Generate a salt
+        const salt = await bcrypt.genSalt(10);
+
+        // Hash the password with the salt
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+
+        // Replace the plain password with the hashed one
+        user.password = hashedPassword;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Method to compare the password during login
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
 
 module.exports = mongoose.model('User', userSchema);
